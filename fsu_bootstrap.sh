65b0c83320728877b377f20ac7f00ab85e494d45
Bryan J. Brown <bryjbrown@gmail.com>
2018-02-09 13:11:05 -0500

    Transferral of custom scripts from islandora7x_aws

---
+++ fsu_bootstrap.sh
@@ -0,0 +1,15 @@
+# Install Drupal modules
+wget https://raw.githubusercontent.com/fsulib/islandora7x_aws/master/FsuCustom/custom_drupal_modules.txt -O /tmp/custom_drupal_modules.txt
+while read line
+do
+  /root/.composer/vendor/bin/drush --root=/var/www/html -y dl $line
+  #/root/.composer/vendor/bin/drush --root=/var/www/html -y en $line
+done < /tmp/custom_drupal_modules.txt
+
+# Install Islandora modules
+wget https://raw.githubusercontent.com/fsulib/islandora7x_aws/master/FsuCustom/custom_islandora_modules.txt -O /tmp/custom_islandora_modules.txt
+while read line
+do
+  git clone https://github.com/$line
+  #/root/.composer/vendor/bin/drush --root=/var/www/html -y en $line
+done < /tmp/custom_islandora_modules.txt
