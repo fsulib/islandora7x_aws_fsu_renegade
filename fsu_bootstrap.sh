25229ef345e1bebf1b05e819947e41c02835a8f4
Bryan J. Brown <bryjbrown@gmail.com>
2018-02-09 13:31:35 -0500

    Adds logging to fsu_custom.sh

--- fsu_bootstrap.sh
+++ fsu_bootstrap.sh
@@ -2,14 +2,21 @@
 wget https://raw.githubusercontent.com/fsulib/islandora7x_aws_fsu_renegade/master/custom_drupal_modules.txt -O /tmp/custom_drupal_modules.txt
 while read line
 do
-  /root/.composer/vendor/bin/drush --root=/var/www/html -y dl $line
+  echo "\nInstalling $line..." >> /root/drupal-installs.txt
+  /root/.composer/vendor/bin/drush --root=/var/www/html -y dl $line >> /root/drupal-installs.txt
+  #echo "\nEnabling $line..." >> /root/drupal-installs.txt
   #/root/.composer/vendor/bin/drush --root=/var/www/html -y en $line
+  echo "\n" >> /root/drupal-installs.txt
 done < /tmp/custom_drupal_modules.txt
 
+
 # Install Islandora modules
-wget https://raw.githubusercontent.com/fsulib/islandora7x_aws_fsu_renegade/master/FsuCustom/custom_islandora_modules.txt -O /tmp/custom_islandora_modules.txt
+wget https://raw.githubusercontent.com/fsulib/islandora7x_aws_fsu_renegade/master/custom_islandora_modules.txt -O /tmp/custom_islandora_modules.txt
 while read line
 do
-  git clone https://github.com/$line
+  echo "\nInstalling $line..." >> /root/islandora-installs.txt
+  git clone https://github.com/$line >> /root/islandora-installs.txt
+  #echo "\nEnabling $line..." >> /root/islandora-installs.txt
   #/root/.composer/vendor/bin/drush --root=/var/www/html -y en $line
+  echo "\n" >> /root/islandora-installs.txt
 done < /tmp/custom_islandora_modules.txt
